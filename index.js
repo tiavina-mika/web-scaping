f0c40e6569c583db7cc16989d1b83a2695a9f70f
tiavina-mika <ralainirinatiavina@gmail.com>
2020-06-19 15:23:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import mongoose from 'mongoose'
 import { scrapConversations } from './scrap/conversations'
 
-// const dbUrl = 'mongodb+srv://admin:flibagoumo0@cluster0-y6yfn.gcp.mongodb.net/test?readPreference=primary&ssl=true&replicaSet=Cluster0-shard-0&authSource=admin'
 const dbUrl = 'mongodb://admin:flibagouMO0@cluster0-shard-00-00-y6yfn.gcp.mongodb.net:27017,cluster0-shard-00-01-y6yfn.gcp.mongodb.net:27017,cluster0-shard-00-02-y6yfn.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
 // const dbUrl = 'mongodb://localhost/demo_scraping'
 const run = async () => {
